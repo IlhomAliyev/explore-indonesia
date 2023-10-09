e01606e8c7a2f73bdb9aad19917138dba3f3b582
TehnikumCodingFrontend <tehnikum.coding.frontend@gmail.com>
2023-10-09 16:35:01 +0500

    some fixes

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,56 +1,62 @@
-'use strict';
+"use strict";
 
-import sliderModule from './sliders.js';
+import sliderModule from "./sliders.js";
 sliderModule.allSliders();
 
-import headerAndMenuBurger from './header-menuBurger.js';
+import headerAndMenuBurger from "./header-menuBurger.js";
 headerAndMenuBurger.allFunctions();
 
-import accordionModule from './accordion.js';
+import accordionModule from "./accordion.js";
 accordionModule.accordion();
 
-import searchByPage from './searchByPage.js';
+import searchByPage from "./searchByPage.js";
 searchByPage.searchByPage();
 
+//! fix videos autoplay on macOS
+document.addEventListener("DOMContentLoaded", () => {
+  const backgroundVideos = document.querySelectorAll(".background-video");
+  backgroundVideos.forEach((bgcV) => bgcV.play());
+});
+
 //! VIDEO-PLAY-BUTTONS
-const playButtons = document.getElementsByClassName('play-button');
+const playButtons = document.getElementsByClassName("play-button");
 
 if (playButtons.length > 0) {
   for (const eachPlayButton of playButtons) {
-      eachPlayButton.addEventListener("click", playVideo);
-      function playVideo(event) {
-          eachPlayButton.style.display = "none";
-          let exactVideo = eachPlayButton.previousElementSibling;
-          exactVideo.play();
-          exactVideo.setAttribute("controls", true);
-      }
+    eachPlayButton.addEventListener("click", playVideo);
+    function playVideo() {
+      eachPlayButton.style.display = "none";
+      let exactVideo = eachPlayButton.previousElementSibling;
+      exactVideo.play();
+      exactVideo.setAttribute("controls", true);
+    }
   }
 }
 
 //! СКРОЛЛ ДО САМОЙ ВЕРХНЕЙ ТОЧКИ
-const buttonUp = document.querySelector('.icon-button-up');
-buttonUp.addEventListener('click', function () {
+const buttonUp = document.querySelector(".icon-button-up");
+buttonUp.addEventListener("click", function () {
   window.scrollTo({
     top: 0,
-    behavior: 'smooth',
+    behavior: "smooth",
   });
 });
 
 //! ПОИСК
-const menuBody = document.querySelector('.menu-body');
-const searchButton = document.querySelector('.icon-search-icon');
-const searchInput = document.querySelector('#search-input');
+const menuBody = document.querySelector(".menu-body");
+const searchButton = document.querySelector(".icon-search-icon");
+const searchInput = document.querySelector("#search-input");
 
-searchButton.addEventListener('click', function () {
-  searchInput.classList.toggle('_active');
+searchButton.addEventListener("click", function () {
+  searchInput.classList.toggle("_active");
   setTimeout(focusOnSearchInput, 100);
   //? Закрываем меню, если оно открыто
   if (
-    menuBody.classList.contains('_active') ||
-    document.body.classList.contains('_lock')
+    menuBody.classList.contains("_active") ||
+    document.body.classList.contains("_lock")
   ) {
-    menuBody.classList.remove('_active');
-    document.body.classList.remove('_lock');
+    menuBody.classList.remove("_active");
+    document.body.classList.remove("_lock");
   }
 });
 
@@ -59,29 +65,29 @@ function focusOnSearchInput() {
 }
 
 //! POP-UP
-// const popUp = document.querySelector('.pop-up');
-// const closeButton = document.querySelector('.close-button');
-// let popUpCounter = 0;
+const popUp = document.querySelector(".pop-up");
+const closeButton = document.querySelector(".close-button");
+let popUpCounter = 0;
 
-// document.addEventListener('mouseleave', showPopUp);
-// closeButton.addEventListener('click', hidePopUp);
-// window.addEventListener('keyup', hidePopUp);
+document.addEventListener("mouseleave", showPopUp);
+closeButton.addEventListener("click", hidePopUp);
+window.addEventListener("keyup", hidePopUp);
 
-// function showPopUp() {
-//   if (popUpCounter < 2) {
-//     popUp.classList.add('_active');
-//     document.body.classList.add('_popUp');
-//   }
-//   popUpCounter++;
-// }
+function showPopUp() {
+  if (popUpCounter < 2) {
+    popUp.classList.add("_active");
+    document.body.classList.add("_popUp");
+  }
+  popUpCounter++;
+}
 
-// function hidePopUp(event) {
-//   if (popUp && popUp.classList.contains('_active')) {
-//     popUp.classList.remove('_active');
-//     document.body.classList.remove('_popUp');
-//     if (event.code == 'Escape') {
-//       popUp.classList.remove('_active');
-//       document.body.classList.remove('_popUp');
-//     }
-//   }
-// }
+function hidePopUp(event) {
+  if (popUp && popUp.classList.contains("_active")) {
+    popUp.classList.remove("_active");
+    document.body.classList.remove("_popUp");
+    if (event.code == "Escape") {
+      popUp.classList.remove("_active");
+      document.body.classList.remove("_popUp");
+    }
+  }
+}
